0466caf94f9a98fd9c4573f95b53950a31afa19c
Arsenii <macbook@arseniilaponov.local>
2026-02-25 12:25:37 +0200

    Add category.py and questions.py to homework_5

---
+++ homework/homework_5/questions.py
@@ -0,0 +1,49 @@
+from datetime import datetime
+
+from sqlalchemy import String, Text, DateTime, Boolean, Integer, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from models.base import Base
+
+class Question(Base):
+    __tablename__ = "questions"
+
+    title: Mapped[str] = mapped_column(
+        String(150),
+        nullable=False
+    )
+    description: Mapped[str | None] = mapped_column(
+        Text,
+        nullable=True
+    )
+    start_date: Mapped[datetime] = mapped_column(
+        DateTime,
+        nullable=False
+    )
+    end_date: Mapped[datetime] = mapped_column(
+        DateTime,
+        nullable=False
+    )
+    is_active: Mapped[bool] = mapped_column(
+        Boolean,
+        default=True
+    )
+
+    # Добавляем поле category_id и связь с Category
+    category_id: Mapped[int | None] = mapped_column(
+        Integer,
+        ForeignKey('categories.id'),
+        nullable=True
+    )
+    category = relationship("Category", backref="questions")
+
+
+class QuestionOption(Base):
+    __tablename__ = 'question_options'
+
+    question_id: Mapped[int] = mapped_column(
+        Integer,
+        ForeignKey('questions.id'),
+        nullable=False
+    )
+    text: Mapped[str] = mapped_column(String(255), nullable=False)
